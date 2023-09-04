73ee4428b01a91fe395c4ddf5dc5cd05d0d0e5e6
Ariana <ahhhkadhs@pi>
2023-09-04 10:42:30 -0300

    hasta ej 2

--- Guia-4.hs
+++ Guia-4.hs
@@ -1,5 +1,17 @@
 -- GUIA PRACTICA 4 (Recursion sobre números enteros)
 
+-- Funcion de factorial hecha en clase
+factorial :: Int -> Int
+factorial n | n == 0 = 1
+            | otherwise = n * factorial (n-1)
+
 -- EJERCICIO 1
 fibonacci :: Integer -> Integer
-fibonacci = undefined
+fibonacci n | n == 0 = 0
+            | n == 1 = 1
+            | n > 1 = fibonacci (n-1) + fibonacci (n-2)
+
+-- EJERCICIO 2
+parteEntera :: Float -> Integer
+parteEntera x | 0 < x && x < 1 = 0
+              | otherwise = 1 + parteEntera (x-1)
