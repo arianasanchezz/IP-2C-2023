e162423ade13214ad561e763a3f11a8c1e688ef6
Ariana <ari.sanchezsm@gmail.com>
2023-09-08 22:19:03 -0300

    hice hasta ejercicio palabraMasLarga

--- Guia-5.hs
+++ Guia-5.hs
@@ -26,3 +26,33 @@ sacarEspacioFinal [] = []
 sacarEspacioFinal (x:[]) | (x == ' ') = []
                          | otherwise = [x]
 sacarEspacioFinal (x:xs) = x : sacarEspacioFinal xs
+
+-- c) que dada una lista arma una nueva lista con las palabras de la lista original
+palabras :: [Char] -> [[Char]]
+palabras xs = palabrasAux (listaLimpia xs)
+
+palabrasAux :: [Char] -> [[Char]]
+palabrasAux [] = []
+palabrasAux (x:xs) = primeraPalabra (x:xs) : palabrasAux (sacarPrimeraPalabra (x:xs))
+
+listaLimpia :: [Char] -> [Char]
+listaLimpia xs = sacarEspacioIniFin (sacarEspaciosRepetidos xs)
+
+primeraPalabra :: [Char] -> [Char]
+primeraPalabra [] = []
+primeraPalabra (x:xs) | x == ' ' = []
+                      | otherwise = x : primeraPalabra xs
+
+sacarPrimeraPalabra :: [Char] -> [Char]
+sacarPrimeraPalabra [] = []
+sacarPrimeraPalabra (x:xs) | x == ' ' = xs
+                           | otherwise = sacarPrimeraPalabra xs
+
+-- d) que dada una lista de caracteres devuelve su palabra mas larga
+palabraMasLarga :: [Char] -> [Char]
+palabraMasLarga xs = palabraMasLargaAux (listaLimpia xs)
+
+palabraMasLargaAux :: [Char] -> [Char]
+palabraMasLargaAux (x:xs) | sacarPrimeraPalabra (x:xs) == [] = primeraPalabra (x:xs)
+                          | length (primeraPalabra (x:xs)) > length (primeraPalabra (sacarPrimeraPalabra (x:xs))) = primeraPalabra (x:xs)
+                          | otherwise = palabraMasLargaAux xs
