d9a557cbd87954691d3e9e49646a91fc30b61fd0
Ariana <ari.sanchezsm@gmail.com>
2023-09-11 12:21:42 -0300

    empecé ejercicio 2 guia 5

--- Guia-5.hs
+++ Guia-5.hs
@@ -1,19 +1,47 @@
 -- GUIA PRACTICA 5 (Recursión sobre listas)
 
 -- EJERCICIO 1
--- 1) dada una lista devuelve su cantidad de elementos
+-- 1. dada una lista devuelve su cantidad de elementos
 longitud :: [t] -> Integer
 longitud [] = 0
 longitud (x:xs) = 1 + longitud xs
 
+-- 2.
+ultimo :: [t] -> t
+ultimo = undefined
+
+-- EJERCICIO 2
+-- 1.
+pertenece :: (Eq t) => t -> [t] -> Bool
+pertenece x [] = False
+pertenece x (y:ys) | x == y = True
+                   | otherwise = pertenece x ys
+
+-- 2. dada una lista devuelve verdadero sí y solamente sí todos sus elementos son iguales
+todosIguales :: (Eq t) => [t] -> Bool
+todosIguales (x:[]) = True
+todosIguales (x:y:ys) | (x == y) && (todosIguales (y:ys)) = True
+                      | otherwise = False
+
+-- 3.
+todosDistintos :: (Eq t) => [t] -> Bool
+todosDistintos xs = (hayRepetidos xs) == False
+
+-- 4.
+hayRepetidos :: (Eq t) => [t] -> Bool
+hayRepetidos [] = False
+hayRepetidos (x:xs) | pertenece x xs = True
+                    | otherwise = hayRepetidos xs
+
+
 -- EJERCICIO 4 (hasta inciso 5- hechos en clase del 7/9)
--- 1) reemplaza cada subsecuencia de blancos contiguos de la primera lista por un solo blanco en la segunda lista.
+-- 1. reemplaza cada subsecuencia de blancos contiguos de la primera lista por un solo blanco en la segunda lista.
 sacarEspaciosRepetidos :: [Char] -> [Char]
 sacarEspaciosRepetidos (x:[]) = [x]
 sacarEspaciosRepetidos (x:y:ys) | ((x == y) && (x == ' ')) = sacarEspaciosRepetidos (y:ys)
                                 | otherwise = x : sacarEspaciosRepetidos (y:ys)
 
--- 2) dada una lista de caracteres devuelve la cantidad de palabras que tiene
+-- 2. dada una lista de caracteres devuelve la cantidad de palabras que tiene
 contarPalabras :: [Char] -> Integer
 contarPalabras xs = contarEspacios(sacarEspacioIniFin (sacarEspaciosRepetidos xs)) + 1
 
@@ -33,7 +61,7 @@ sacarEspacioFinal (x:[]) | (x == ' ') = []
                          | otherwise = [x]
 sacarEspacioFinal (x:xs) = x : sacarEspacioFinal xs
 
--- 3) dada una lista arma una nueva lista con las palabras de la lista original
+-- 3. dada una lista arma una nueva lista con las palabras de la lista original
 palabras :: [Char] -> [[Char]]
 palabras xs = palabrasAux (listaLimpia xs)
 
@@ -54,7 +82,7 @@ sacarPrimeraPalabra [] = []
 sacarPrimeraPalabra (x:xs) | x == ' ' = xs
                            | otherwise = sacarPrimeraPalabra xs
 
--- 4) dada una lista de caracteres devuelve su palabra mas larga
+-- 4. dada una lista de caracteres devuelve su palabra mas larga
 palabraMasLarga :: [Char] -> [Char]
 palabraMasLarga xs = palabraMasLargaAux (listaLimpia xs)
 
@@ -63,7 +91,7 @@ palabraMasLargaAux (x:xs) | sacarPrimeraPalabra (x:xs) == [] = primeraPalabra (x
                           | length (primeraPalabra (x:xs)) > length (primeraPalabra (sacarPrimeraPalabra (x:xs))) = primeraPalabra (x:xs)
                           | otherwise = palabraMasLargaAux xs
 
--- 5) a partir de una lista de palabras arma una lista de caracteres concatenandolas
+-- 5. a partir de una lista de palabras arma una lista de caracteres concatenandolas
 aplanar :: [[Char]] -> [Char]
 aplanar [] = []
 aplanar (xs:xss) = xs ++ aplanar xss
