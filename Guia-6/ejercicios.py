7ba04cfd96750d1247efcba6bc588aee200b040e
Ariana <ari.sanchezsm@gmail.com>
2023-10-02 09:54:55 -0300

    comencé guia 6 en clase

---
+++ Guia-6/ejercicios.py
@@ -0,0 +1,15 @@
+import math
+
+#  EJERCICIOS HECHOS EN CLASE PRACTICA 2/10
+# Ejercicio 1.1
+def imprimir_hola_mundo():
+    print("¡Hola mundo!")
+
+# Ejercicio 1.5
+def perimetro() -> float:
+    res:float = 2*math.pi
+    return res
+
+
+# imprimir_hola_mundo()
+print(perimetro())
