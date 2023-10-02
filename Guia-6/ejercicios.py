797d2111174a19311a08d3c67d4540fe3c969b2a
Ariana <ari.sanchezsm@gmail.com>
2023-10-02 11:25:31 -0300

    agregue ejercicios de while y for num in range

--- Guia-6/ejercicios.py
+++ Guia-6/ejercicios.py
@@ -28,6 +28,33 @@ def devolver_el_doble_si_es_par(numero:int) -> int:
     else:
         return numero
 
+# Ejercicio 6.2
+def imprimir_pares():
+    i: int = 10
+    while (i<=40):
+        if (es_multiplo_de(i, 2)):
+            print(i)
+        i += 1
+
+# Ejercicio 6.4
+def cuenta_regresiva(n: int):
+    while (n>=1):
+        print(n)
+        n -= 1
+    print("Despegue")
+
+# Ejercicio 7.2
+def imprimir_pares2():
+    for contador in range(10,41,2):
+        print(contador)
+
+# Ejercicio 7.4
+def cuenta_regresiva2(n: int):
+    for i in range(n,0,-1):
+        print(i)
+    print("Despegue")
+
+
 
 # imprimir_hola_mundo()
 # print(perimetro())
@@ -37,3 +64,7 @@ def devolver_el_doble_si_es_par(numero:int) -> int:
 # print(es_nombre_largo("Marianela"))
 # print(devolver_el_doble_si_es_par(2))
 # print(devolver_el_doble_si_es_par(3))
+# imprimir_pares()
+# cuenta_regresiva(10)
+# imprimir_pares2()
+# cuenta_regresiva2(10)
